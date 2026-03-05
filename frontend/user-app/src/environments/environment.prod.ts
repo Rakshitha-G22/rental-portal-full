f15195af70a8fec3d3a0f4fd09abd9815a2fada6
Rakshitha G <rakshithag227@gmail.com>
2026-03-06 00:28:29 +0530

    make all changes

--- frontend/user-app/src/environments/environment.prod.ts
+++ frontend/user-app/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: "https://rental-portal-production-5ef0.up.railway.app"
+  apiUrl: "https://rental-portal-production-5ef0.up.railway.app/api"
 };
